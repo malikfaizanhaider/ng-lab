4e08987538f35b23f7d360bcc7dfa3e516996fad
malikfaizanhaider <malikfaizanhaider@gmail.com>
2024-01-20 02:12:44 +0500

    Initial Layout: App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {UnstyledDrawerComponent} from "@unstyled/components/drawer/drawer.component";
 import {OverlayComponent} from "../@unstyled/components/overlay/overlay.component";
+import {CdkScrollable} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UnstyledDrawerComponent, OverlayComponent],
+  imports: [CommonModule, RouterOutlet, UnstyledDrawerComponent, OverlayComponent, CdkScrollable],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'unstyled';
+
+  numbers = Array.from({length: 100}, (_, i) => i + 1);
 
   drawerMode = 'side';
   drawerOpened = true;
