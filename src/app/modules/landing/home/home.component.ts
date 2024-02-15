377dc03a6afe6497ead1e02853b7344f1f602954
Faizan Haider <fhaider@i2cinc.com>
2024-02-15 23:12:34 +0500

    Upgrading.

--- src/app/modules/landing/home/home.component.ts
+++ src/app/modules/landing/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {HomeService} from "./home.service";
-import {Root} from "./types";
-import {NgClass, NgForOf, NgIf, NgTemplateOutlet} from "@angular/common";
+import {Daum, NavChildren, Root} from "./types";
+import {isPlatformBrowser, NgClass, NgForOf, NgIf, NgTemplateOutlet} from "@angular/common";
 import {CdkCellDef, CdkHeaderCellDef, CdkRowDef, CdkTable} from "@angular/cdk/table";
 import {AgGridAngular} from "ag-grid-angular";
+import {catchError, of} from "rxjs";
+import {RowSelectedEvent} from "ag-grid-community";
 
 @Component({
   selector: 'app-home',
@@ -23,59 +25,190 @@ import {AgGridAngular} from "ag-grid-angular";
   styleUrl: './home.component.scss',
   providers: [HomeService]
 })
-export class HomeComponent {
-  rowData: any[];
-
-  columnDefs = [
-    { field: 'name' },
-    { field: 'perma_link' },
-    { field: 'nav_parent' },
-    { field: 'title' },
-    { field: 'nav_type' },
-    { field: 'created_by_fname' },
-    { field: 'last_updated_by_fname' },
-    { field: 'added_on' },
-    { field: 'last_updated_on' },
-    { field: 'is_active' },
-  ];
+export class HomeComponent implements OnInit{
+  data: any;
+  gridOptions: any;
+  deleteRow: boolean = false;
+  selection: string | null = null;
 
   defaultColDef = {
     flex: 1,
-    minWidth: 150,
+    minWidth: 200,
+    resizable: false,
+    wrapText: false,
+    sortable: false,
   };
 
-  data: any;
-  total: number = 0;
+  constructor(private homeService: HomeService,
+              @Inject(PLATFORM_ID) private platformId: any,) {
+    this.onRowSelected = this.onRowSelected.bind(this);
+    this.onSelectionChanged = this.onSelectionChanged.bind(this);
+  }
 
-  constructor(private homeService: HomeService) {
-    this.getData();
-    this.rowData = this.createRowData();
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.getData();
+      this.gridOptions.getGridOption();
+    }
   }
 
   getData() {
-    this.homeService.getData().subscribe((resp: Root) => {
-      this.data = resp?.data;
+    this.homeService.getData()
+      .pipe(
+        catchError((error: any) => {
+          console.error('Error fetching data', error);
+          return of(null);
+        })
+      )
+      .subscribe({
+        next: (response: Root) => {
+          if (response?.data) {
+            this.data = this.processData(response.data);
+            this.gridOptions = {
+              columnDefs: [
+                {
+                  headerName: 'Navigation Name',
+                  field: 'name',
+                  pinned: 'left',
+                  checkboxSelection: true,
+                  width: 325,
+                  wrapText: false,
+                  cellStyle: params => {
+                    if (params.data.nav_parent === null || params.data.nav_parent === '-' || params.data.nav_children.length > 0) {
+                      return {
+                        'font-weight': 'bold',
+                      };
+                    }
 
-      this.rowData = this.data?.data;
-    });
+                    if (params.data.nav_parent !== null) {
+                      return {
+                        'color': 'var(--i2c-color-warning-500)',
+                      };
+                    }
+
+                    return null;
+                  },
+                },
+                {
+                  field: 'perma_link',
+                  headerName: 'Permalink',
+                  width: 225,
+                },
+                {
+                  field: 'nav_parent',
+                  headerName: 'Nav Parent',
+                },
+                {
+                  field: 'title',
+                  headerName: 'Title',
+                },
+                {
+                  field: 'nav_type',
+                  headerName: 'Nav Type',
+                },
+                {
+                  field: 'created_by_fname',
+                  headerName: 'Created By',
+                  valueFormatter: (params: any) => `${params.data.created_by_fname} ${params.data.created_by_lname}`,
+                },
+                {
+                  field: 'last_updated_by_fname',
+                  width: 100,
+                  headerName: 'Updated By',
+                  valueFormatter: (params: any) => {
+                    const {last_updated_by_fname, last_updated_by_lname} = params.data;
+                    const value = `${last_updated_by_fname || ''} ${last_updated_by_lname || ''}`.trim();
+                    return value ? value : '-';
+                  }
+                },
+                {
+                  field: 'added_on',
+                  headerName: 'Updated On',
+                },
+                {
+                  field: 'last_updated_on',
+                  headerName: 'Updated On',
+                },
+                {
+                  field: 'is_active',
+                  headerName: 'Status',
+                }
+              ],
+              rowSelection: 'single',
+              rowData: this.processData(this.data),
+              defaultColDef: this.defaultColDef,
+              getRowStyle: function (params) {
+                if (params.data.nav_parent === null || params.data.nav_parent === '-') {
+                  return {
+                    background: 'lightgray',
+                  };
+                }
+                return null;
+              },
+              onRowSelected: this.onRowSelected,
+              onSelectionChanged: this.onSelectionChanged,
+            };
+          }
+        },
+        error: (error: any) => {
+          console.error('Error fetching data', error);
+        },
+        complete: () => {
+        }
+      });
   }
 
-  createRowData() {
-    const rowData: any[] = [];
-    for (let i = 0; i < 100; i++) {
-      rowData.push({
-        name: 'Name ' + i,
-        perma_link: 'Perma Link ' + i,
-        nav_parent: 'Nav Parent ' + i,
-        title: 'Title ' + i,
-        nav_type: 'Nav Type ' + i,
-        created_by_fname: 'Created By Fname ' + i,
-        last_updated_by_fname: 'Last Updated By Fname ' + i,
-        added_on: 'Added On ' + i,
-        last_updated_on: 'Last Updated On ' + i,
-        is_active: 'Is Active ' + i,
+  processData(data: Daum[]): any[] {
+    const flattenTree = (item: NavChildren): any[] => {
+      let result = [item];
+      if (item.nav_children) {
+        item.nav_children.forEach(child => {
+          result = [...result, ...flattenTree(child)];
+        });
+      }
+      return result;
+    };
+
+    return data.flatMap(parent => flattenTree(parent));
+  }
+
+  onRowSelected(event: any) {
+    const selectedRow = event.node.data; // Access the selected row data
+   this.getRowData(selectedRow)
+  }
+
+  onSelectionChanged(event: any) {
+    const selectedRows = this.gridOptions.api.getSelectedRows(event);
+    this.getRowData(selectedRows);
+  }
+
+  /**
+   * @method getRowData
+   * @param data
+   * @returns {string}
+   * @description Gets the data of the selected row.
+   */
+  getRowData(data?: any): string {
+    this.selection = data;
+    console.log('Selected Row Data:', this.selection);
+    return this.selection;
+  }
+
+  calculateLevels(data: Daum[]): { [parentId: string]: number } {
+    const levels: { [parentId: string]: number } = {};
+
+    const calculate = (items: NavChildren[], level: number = 0, parentId: string = null) => {
+      items.forEach(item => {
+        if (!levels[item.nav_parent] || level > levels[item.nav_parent]) {
+          levels[item.nav_parent] = level;
+        }
+        if (item.nav_children) {
+          calculate(item.nav_children, level + 1, item.numbr);
+        }
       });
-    }
-    return rowData;
+    };
+
+    calculate(data);
+    return levels;
   }
 }
